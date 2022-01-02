9c860a19c7caef5cc6d3cc92d80a0c5b7c6dd587
RobinBachus <robinbachus@gmail.com>
2022-01-02 05:58:47 +0100

    better logs + changed text + delete old messages

---
+++ changelog.md
@@ -0,0 +1,8 @@
+# This changelog is in broad terms and might miss a few changes here and there
+
+V0.1:
+
+- bot deletes all of its own previous messages on restart to avoid clutter
+- added initial welcome/role message
+- added commands: "status", "info" and "help"
+- added server log messages
--- index.js
+++ index.js
@@ -32,49 +32,80 @@ const client = new discord_js_1.default.Client({
     ]
 });
 client.on('ready', () => {
-    console.log(getTime() + ': Bot startup...');
-    const channel = client.channels.cache.get('926445404072058880');
+    sendLog("Bot startup...");
+    const channel = client.channels.cache.get('927026779628126228');
+    // clear bot messages on roles channel
     if (channel !== undefined) {
-        channel.send('💦 Sea of Thieves\n❤ Among Us\n🥰 Dead By Daylight').then(sentEmbed => {
+        channel.messages.fetch()
+            .then((messages) => {
+            let messageList = messages.filter((m) => m.author.id === '926211660849500190');
+            for (let i of messageList) {
+                channel.messages.delete(i[0]);
+            }
+            sendLog(`deleted ${messageList.size} messages`);
+        });
+    }
+    // send initial role message on startup
+    if (channel !== undefined) {
+        channel.send("Use 'RAB info' for more information\n\
+Use 'RAB help' for more commands\n\n\
+Click on the emoji to get the corresponding role\n\n\
+Roles:\n\
+    💦 Sea of Thieves\n\
+    ❤ Among Us\n\
+    🥰 Dead By Daylight").then((sentEmbed) => {
             sentEmbed.react("💦");
             sentEmbed.react("❤");
             sentEmbed.react("🥰");
         });
     }
-    console.log(getTime() + ": Bot ready");
+    sendLog("Bot ready");
 });
 client.on('messageCreate', (message) => {
     var _a;
-    if (message.content === 'RoleAssignBot status') {
+    if (message.content === 'RAB status') {
         message.reply({
             content: 'currently running!'
         });
-        console.log(getTime() + ": replied to 'status' command from '" + message.author.username + "' on '" + ((_a = message.guild) === null || _a === void 0 ? void 0 : _a.name) + "'");
+        sendLog("replied to 'status' command from '" + message.author.username + "' on '" + ((_a = message.guild) === null || _a === void 0 ? void 0 : _a.name) + "'");
     }
 });
 client.on('messageCreate', (message) => {
     var _a;
-    if (message.content === 'RoleAssignBot info') {
+    if (message.content === 'RAB info') {
         message.reply({
-            content: "Made by Robin Bachus\nCheck my source code here: github.com/RobinBachus/DiscordRoleBot\n\nSorry for spaghetti code 🙃"
+            content: `Made by Robin Bachus\nVersion: ${getVersion()}\nCheck my source code here: github.com/RobinBachus/DiscordRoleBot\n\nSorry for spaghetti code 🙃`
         });
-        console.log(getTime() + ": replied to 'info' command from '" + message.author.username + "' on '" + ((_a = message.guild) === null || _a === void 0 ? void 0 : _a.name) + "'");
+        sendLog("replied to 'info' command from '" + message.author.username + "' on '" + ((_a = message.guild) === null || _a === void 0 ? void 0 : _a.name) + "'");
     }
 });
 client.on('messageCreate', (message) => {
     var _a;
-    if (message.content === 'RoleAssignBot help') {
+    if (message.content === 'RAB help') {
         message.reply({
-            content: "- RoleAssignBot info:     info about the bot\n- RoleAssignBot status:     check if the bot is running\n"
+            content: "- RAB info:     info about the bot\n- RAB status:     check if the bot is running\n"
         });
-        console.log(getTime() + ": replied to 'help' command from '" + message.author.username + "' on '" + ((_a = message.guild) === null || _a === void 0 ? void 0 : _a.name) + "'");
+        sendLog("replied to 'help' command from '" + message.author.username + "' on '" + ((_a = message.guild) === null || _a === void 0 ? void 0 : _a.name) + "'");
     }
 });
 function getTime() {
     let date = new Date();
-    let today = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-    let now = `${date.getHours() + 1}:${date.getMinutes()}:${date.getSeconds()}`; // added "+1" to hour due to server time difference
+    let today = date.toLocaleDateString('en-BE', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    let now = date.toLocaleTimeString("en-BE", { timeZone: "Europe/Brussels", hour: '2-digit', minute: '2-digit', second: '2-digit' });
     let time = today + ' ' + now;
     return time;
+}
+function sendLog(message) {
+    console.log(`${getTime()}: ${message}`);
+}
+function getVersion() {
+    const fs = require('fs');
+    try {
+        const data = fs.readFileSync("version.txt", 'utf8');
+        return (data);
+    }
+    catch (err) {
+        console.error(err);
+    }
 }
 client.login(process.env.TOKEN);
